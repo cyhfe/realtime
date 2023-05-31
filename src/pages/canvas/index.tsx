598ef048f8ffcaff946151247c377d9952954c7c
chenyuhao <cccchenyuhao@gmail.com>
2023-06-01 00:17:25 +0800

    canvas style

--- src/pages/canvas/index.tsx
+++ src/pages/canvas/index.tsx
@@ -1,8 +1,15 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { io } from "socket.io-client";
-import { AUTH_BASE_URL } from "../../utils/const";
+import { AUTH_BASE_URL, COLOR } from "../../utils/const";
 import { useAuth } from "../../context/Auth";
 import { useThrottle } from "../../hooks/useThrottle";
+import { PenIcon, TrashIcon } from "./icon";
 interface Current {
   x: number;
   y: number;
@@ -15,6 +22,8 @@ interface Line {
   ny: number;
 }
 
+const colors = [COLOR.Green, COLOR.Red, COLOR.Slate, COLOR.Violet];
+
 function Canvas() {
   const user = useAuth();
   const socketRef = useRef(null);
@@ -22,7 +31,7 @@ function Canvas() {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const [isDrawing, setIsDrawing] = useState(false);
   const [current, setCurrent] = useState<Current | null>(null);
-
+  const [strokeColor, setStrokeColor] = useState(COLOR.Slate);
   // reflow
   useLayoutEffect(() => {
     function onResize() {
@@ -37,19 +46,29 @@ function Canvas() {
     };
   }, []);
 
-  function draw(line: Line) {
-    const { px, py, nx, ny } = line;
+  const draw = useCallback(
+    function draw(line: Line) {
+      const { px, py, nx, ny } = line;
+      const el = canvasRef.current;
+      const ctx = el.getContext("2d");
+      const { width, height } = canvasRef.current.getBoundingClientRect();
+      ctx.beginPath();
+      ctx.moveTo(px * width, py * height);
+      ctx.lineTo(nx * width, ny * height);
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = strokeColor;
+      ctx.stroke();
+      ctx.closePath();
+      setCurrent({ x: nx, y: ny });
+    },
+    [strokeColor]
+  );
+
+  function clear() {
     const el = canvasRef.current;
     const ctx = el.getContext("2d");
     const { width, height } = canvasRef.current.getBoundingClientRect();
-    ctx.beginPath();
-    ctx.moveTo(px * width, py * height);
-    ctx.lineTo(nx * width, ny * height);
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "red";
-    ctx.stroke();
-    ctx.closePath();
-    setCurrent({ x: nx, y: ny });
+    ctx.clearRect(0, 0, width, height);
   }
 
   function onMouseDown(e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
@@ -100,16 +119,61 @@ function Canvas() {
     const socket = socketRef.current;
 
     socket.on("drawing", draw);
+    socket.on("clear", clear);
+    socket.on("changeStrokeColor", setStrokeColor);
     socket.connect();
 
     return () => {
       socket.off("drawing", draw);
+      socket.off("clear", clear);
+
       socket.disconnect();
     };
-  }, [user]);
+  }, [draw, user]);
 
   return (
-    <div className="full-height  border" ref={containerRef}>
+    <div className="full-height  relative border" ref={containerRef}>
+      <div className="absolute  right-1/2 top-5 flex translate-x-1/2 items-center gap-x-1 divide-x  bg-slate-50 p-2 text-slate-400 shadow-xl">
+        <div className="flex items-center pr-1">
+          <button className=" block  p-1">
+            <PenIcon />
+          </button>
+        </div>
+        <div className=" flex items-center gap-x-1 px-1">
+          {colors.map((color) => {
+            return (
+              <button
+                className="box-border rounded-md p-2"
+                key={color}
+                style={{
+                  background: strokeColor === color ? "#cbd5e1" : "",
+                }}
+                onClick={() => {
+                  setStrokeColor(color);
+                  socketRef.current.emit("changeStrokeColor", color);
+                }}
+              >
+                <div
+                  className="h-4 w-4 rounded-full"
+                  style={{
+                    background: color,
+                  }}
+                ></div>
+              </button>
+            );
+          })}
+        </div>
+        <div className="flex items-center pl-1">
+          <button
+            onClick={() => {
+              clear();
+              socketRef.current.emit("clear");
+            }}
+          >
+            <TrashIcon />
+          </button>
+        </div>
+      </div>
       <canvas
         className="bg-white"
         ref={canvasRef}
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,2 +1,9 @@
 export const BASE_URL = process.env.BASE_URL;
 export const AUTH_BASE_URL = process.env.AUTH_BASE_URL;
+
+export const enum COLOR {
+  Slate = "#475569",
+  Red = "#dc2626",
+  Green = "#16a34a",
+  Violet = "#7c3aed",
+}
