30e2e18b000752ab230ff5422b70e5d309e446ee
chenyuhao <cccchenyuhao@gmail.com>
2023-06-05 13:04:29 +0800

    todo

--- src/pages/music/index.tsx
+++ src/pages/music/index.tsx
@@ -1,21 +1,121 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect, useRef, useState } from "react";
+import { Midi } from "@tonejs/midi";
+import * as Tone from "tone";
+const synth = new Tone.PolySynth(Tone.Synth, {
+  envelope: {
+    attack: 0.02,
+    decay: 0.1,
+    sustain: 0.3,
+    release: 1,
+  },
+}).toDestination();
 
 export function Music() {
+  const [midi, setMidi] = useState<any>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  useEffect(() => {
+    async function parseMidi() {
+      const midi = await Midi.fromUrl("/midi/chopin_op27_1.mid");
+      //get the tracks
+      setMidi(midi);
+    }
+    parseMidi();
+  }, []);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+    const scrollHeight = container.scrollHeight;
+    const clientHeight = container.clientHeight;
+    container.scrollTo(0, scrollHeight - clientHeight);
+  }, []);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container || !isPlaying) return;
+    const scrollHeight = container.scrollHeight;
+    const clientHeight = container.clientHeight;
+    function update() {
+      if (!container) return;
+      console.log("update");
+      const offset = container.scrollTop;
+      container.scrollTo(0, offset - 1);
+      requestAnimationFrame(update);
+    }
+    const raf = requestAnimationFrame(update);
+    return () => {
+      cancelAnimationFrame(raf);
+    };
+  }, [isPlaying]);
+
   return (
     <Fullscreen>
       <div className="flex h-full flex-col">
-        <div className="shrink-0 grow-0 bg-white">header</div>
+        <div className="shrink-0 grow-0 bg-white">
+          <button
+            onClick={async () => {
+              if (!midi) return;
+
+              await Tone.start();
+              console.log(midi);
+              midi.tracks.forEach((track: any) => {
+                //tracks have notes and controlChanges
+                //notes are an array
+
+                const notes = track.notes;
+                if (!notes) return;
+                notes.forEach((note: any) => {
+                  //note.midi, note.time, note.duration, note.name
+                  // console.log(note, Tone.now());
+                  synth.triggerAttackRelease(
+                    note.name,
+                    note.duration,
+                    note.time,
+                    note.velocity
+                  );
+                });
+
+                //the control changes are an object
+                //the keys are the CC number
+                // track.controlChanges[64];
+                //they are also aliased to the CC number's common name (if it has one)
+                track.controlChanges.sustain?.forEach((cc: any) => {
+                  // cc.ticks, cc.value, cc.time
+                  console.log(cc, "cc");
+                });
+
+                //the track also has a channel and instrument
+                //track.instrument.name
+                console.log(track.instrument.name);
+              });
+            }}
+          >
+            play
+          </button>
+        </div>
         <div className="flex grow flex-col overflow-hidden bg-slate-200">
-          <div className="grow overflow-y-scroll">
-            <div
-              style={{
-                height: "1000px",
-              }}
-              // className="overflow-y-scroll"
-            >
-              canvas
+          <div className="grow overflow-hidden">
+            <div className="relative h-full w-full">
+              <div
+                className="absolute bottom-0 left-0 right-0 top-0 overflow-y-scroll"
+                ref={containerRef}
+                onScroll={(e: any) => {
+                  const el = e.target;
+                  console.log(el.clientHeight, el.scrollHeight, el.scrollTop);
+                }}
+              >
+                <canvas
+                  ref={canvasRef}
+                  width={800}
+                  height={1000}
+                  className="grow  bg-rose-400"
+                />
+              </div>
             </div>
           </div>
+
           <div className="shrink-0 grow-0 bg-white">keyboard</div>
         </div>
       </div>
--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 const Dotenv = require("dotenv-webpack");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -62,6 +63,9 @@ module.exports = {
     }),
     new WebpackManifestPlugin(),
     new Dotenv(),
+    new CopyPlugin({
+      patterns: [{ from: "public/midi", to: "midi" }],
+    }),
   ],
   resolve: {
     // 尝试按顺序解析这些后缀名。如果有多个文件有相同的名字，但后缀名不同，webpack 会解析列在数组首位的后缀的文件 并跳过其余的后缀。
