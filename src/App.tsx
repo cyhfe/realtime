ae04a3f4b9a3bee6956fc3b16de118d5f7a9e34b
chenyuhao <cccchenyuhao@gmail.com>
2023-06-15 22:50:39 +0800

    webrtc dev env

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { AuthProvider } from "./context/Auth";
 import Router from "./Router";
-
+import WebRtc from "./pages/webrtc";
 export default function App() {
   return (
-    <AuthProvider>
-      <Router />
-    </AuthProvider>
+    // <AuthProvider>
+    //   <Router />
+    // </AuthProvider>
+    <WebRtc />
   );
 }
